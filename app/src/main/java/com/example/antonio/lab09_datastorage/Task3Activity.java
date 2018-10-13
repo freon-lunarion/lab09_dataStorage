0d62ab77f0d68324e31f4f08920f829cf640dc5e
freon-lunarion <antonio.daniswara@gmail.com>
2018-10-14 10:29:01 +1100

    Initial commit task 1 done

---
+++ app/src/main/java/com/example/antonio/lab09_datastorage/Task3Activity.java
@@ -0,0 +1,13 @@
+package com.example.antonio.lab09_datastorage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Task3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_task3 );
+    }
+}
